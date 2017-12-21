c3ce05516c8ee93394c8eb3742c6728a31f7317e
Unknown <gianluca.travasci@gmail.com>
2017-12-21 14:42:17 +0100

    Added Helper
    Con conversione Int to Hex

---
+++ helper.cpp
@@ -0,0 +1,24 @@
+#include "helper.h"
+
+Helper::Helper()
+{
+    hexList << "0" << "1" << "2" << "3" << "4" << "5" << "6" <<
+                       "7" << "8" << "9" << "A" << "B" << "C" <<
+                       "D" << "E" << "F";
+};
+
+
+/***
+    Helper *helper = new Helper();
+    QString convertito = helper->decToHex(toConvert);
+ */
+QString Helper::decToHex(int decimal) {
+    QString hexVal = "";
+    while (decimal > 15) {
+        int rest = decimal % 16;
+        decimal = decimal / 16;
+        hexVal = Helper::hexList.at(rest) + hexVal;
+    }
+    hexVal = Helper::hexList.at(decimal) + hexVal;
+    return hexVal;
+}
--- progetto.pro
+++ progetto.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        progetto.cpp
+        progetto.cpp \
+    helper.cpp
 
-HEADERS  += progetto.h
+HEADERS  += progetto.h \
+    helper.h
 
 FORMS    += progetto.ui
